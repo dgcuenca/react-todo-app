2b40918086dbae873730904548af06ee9ee8571e
diego <dgcuenca@gmail.com>
2023-01-16 14:39:39 -0500

    Add an onChange handler to keep track of changes

--- src/components/todoItem.js
+++ src/components/todoItem.js
@@ -2,7 +2,12 @@ import React from 'react';
 class TodoItem extends React.Component {
   render() {
     return <li>
-      <input type="checkbox" checked={this.props.todo.completed} /> {this.props.todo.title}
+      <input
+        type="checkbox"
+        checked={this.props.todo.completed}
+        onChange={() => console.log("clicked")}
+      />
+      {this.props.todo.title}
     </li>
   }
 }
