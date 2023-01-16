1d49386a163dbb93c74f945d83c66f9ce55d47c2
diego <dgcuenca@gmail.com>
2023-01-16 11:29:56 -0500

    Activate checks, logs warning at runtime

--- src/index.js
+++ src/index.js
@@ -2,4 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TodoContainer from './components/todoContainer';
 
-ReactDOM.render(<TodoContainer />, document.getElementById("root"));
+ReactDOM.render(
+    <React.StrictMode>
+        <TodoContainer />
+    </React.StrictMode>,
+    document.getElementById("root"));
