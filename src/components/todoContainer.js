46f898deae30ce8431490854ced50dc0ee8f9499
diego <dgcuenca@gmail.com>
2023-01-16 16:25:31 -0500

    Start enabling communications betwween components

--- src/components/todoContainer.js
+++ src/components/todoContainer.js
@@ -21,6 +21,11 @@ class TodoContainer extends React.Component {
       }
     ]
   };
+
+  delTodo = id => {
+    console.log("deleted", id);
+  };
+
   render() {
     return (
       <div>
@@ -28,6 +33,7 @@ class TodoContainer extends React.Component {
         <TodosList
           todos={this.state.todos}
           handleChangeProps={this.handleChange}
+          deleteTodoProps={this.delTodo}
         />
       </div>
     );
