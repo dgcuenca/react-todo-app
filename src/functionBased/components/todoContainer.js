9a0c47df559a7a062ebe7c6412f1715c884dd33e
diego <dgcuenca@gmail.com>
2023-01-17 16:46:59 -0500

    Update return to include these components/pages

--- src/functionBased/components/todoContainer.js
+++ src/functionBased/components/todoContainer.js
@@ -82,20 +82,28 @@ const TodoContainer = () => {
   }
 
   return (
-    <Route exact path="/">
-      <div className="container">
-        <div className="inner">
-          <Header />
-          <InputTodo addTodoProps={addTodoItem} />
-          <TodosList
-            todos={todos}
-            handleChangeProps={handleChange}
-            deleteTodoProps={delTodo}
-            setUpdate={setUpdate}
-          />
+    <>
+      <Route exact path="/">
+        <div className="container">
+          <div className="inner">
+            <Header />
+            <InputTodo addTodoProps={addTodoItem} />
+            <TodosList
+              todos={todos}
+              handleChangeProps={handleChange}
+              deleteTodoProps={delTodo}
+              setUpdate={setUpdate}
+            />
+          </div>
         </div>
-      </div>
-    </Route>
+      </Route>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="*">
+        <NotMatch />
+      </Route>
+    </>
   )
 }
 
