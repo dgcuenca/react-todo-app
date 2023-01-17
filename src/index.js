8b983516e093bbb0564c84041529f942d8311d4e
diego <dgcuenca@gmail.com>
2023-01-17 16:03:43 -0500

    Wrap the top-level component in a router

--- src/index.js
+++ src/index.js
@@ -2,9 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TodoContainer from './functionBased/components/todoContainer';
 import "./functionBased/app.css"
+import { BrowserRouter } from "react-router-dom"
 
 ReactDOM.render(
     <React.StrictMode>
-        <TodoContainer />
+        <BrowserRouter>
+            <TodoContainer />
+        </BrowserRouter>
     </React.StrictMode>,
-    document.getElementById("root"));
+    document.getElementById("root")
+);
