528a8da3b211c601b4d43e60bd09de33ee793783
diego <dgcuenca@gmail.com>
2023-01-17 19:31:33 -0500

    Add navbar

--- src/functionBased/components/App.js
+++ src/functionBased/components/App.js
@@ -2,13 +2,17 @@ import { Route, Routes } from 'react-router-dom';
 import About from '../pages/about';
 import NotMatch from '../pages/notMatch';
 import TodoContainer from './todoContainer';
+import Navbar from './navbar';
 
 export default function App() {
   return (
+    <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<TodoContainer />} />
-        <Route path="/about" element={<About />} /> 
+        <Route path="/about" element={<About />} />
         <Route path="*" element={<NotMatch />} />
       </Routes>
+    </>
   );
 }
