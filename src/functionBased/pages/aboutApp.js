bf2fbd506bf881623c1f3cd3fbbb2677cec9a99d
diego <dgcuenca@gmail.com>
2023-01-17 22:06:32 -0500

    Create pages for nested routes

---
+++ src/functionBased/pages/aboutApp.js
@@ -0,0 +1,6 @@
+import React from "react"
+
+const AboutApp = () => {
+  return <div>hello from about APP</div>
+}
+export default AboutApp
---
+++ src/functionBased/pages/aboutAuthor.js
@@ -0,0 +1,6 @@
+import React from "react"
+
+const AboutAuthor = () => {
+  return <div>hello from about Author</div>
+}
+export default AboutAuthor
